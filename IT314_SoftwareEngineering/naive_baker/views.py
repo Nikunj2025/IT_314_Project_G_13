dd23cd72d1753af986ecbe36b3dcb9ce3cce1f86
Rohan Mistry <94956818+Rohan-231@users.noreply.github.com>
2023-11-18 17:30:15 +0530

    Contact Us  and  Upload recipe functions

--- IT314_SoftwareEngineering/naive_baker/views.py
+++ IT314_SoftwareEngineering/naive_baker/views.py
@@ -74,3 +74,40 @@ def signup(request) :
             messages.info(request,'Sign Up is Done Successfully,Please Login')
             return redirect('/signup')
         return render(request,'signup.html')
+    
+def addRecipe(request) :
+    # if request.user.is_anonymous:
+    #     return redirect("/login")
+    if request.method == "POST":
+
+        recipe = Recipe()
+        recipe.name = request.POST.get('recipeName')
+        recipe.ingredients = request.POST.get('list_of_ingre')
+        recipe.instructions = request.POST.get('steps')
+        recipe.recipe_time = request.POST.get('recipeTime')
+        recipe.vegitarity = request.POST.get('vegitarity')
+        recipe.category = request.POST.get('category')
+        recipe.meal_time = request.POST.get('mealtime')
+        recipe.owner = request.user
+        if len(request.FILES) != 0:
+            recipe.image = request.FILES['image']
+        recipe.save()
+        messages.success(request, "Recipe is added successfully!!!")
+        redirect('/addRecipe')
+
+    return render(request,'addRecipe.html')
+
+def contact(request) :
+        # if request.user.is_anonymous:
+        #     return redirect("/login")
+        if request.method == "POST":
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            recipe_name = request.POST.get('recipe_name')
+            phone = request.POST.get('phone')
+            feedback = request.POST.get('message')
+            contact = Contact(name=name,email=email,recipe_name = recipe_name,phone = phone,feedback=feedback,date = datetime.today())
+            contact.save()
+            messages.success(request, "Thanks for your valuable feedback")
+            redirect('/contactus')
+        return render(request,'contact.html')
